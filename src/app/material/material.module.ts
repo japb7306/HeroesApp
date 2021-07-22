9a14a788fc1ec49020768c7925257492ceaec84f
Juan Antonio Puga <juan.antonio.puga@gmail.com>
2021-07-22 14:12:48 -0700

    sidebar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   exports:[
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
     MatSidenavModule,
     MatToolbarModule
+    
   ]
 })
 
